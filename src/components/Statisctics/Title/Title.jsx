462cd92ebea186c43d9cb7ecbd96950f18ebb2c5
kkasyan <104075234+kkasyan@users.noreply.github.com>
2023-01-04 18:10:38 +0200

    Update Title.jsx

--- src/components/Statisctics/Title/Title.jsx
+++ src/components/Statisctics/Title/Title.jsx
@@ -2,9 +2,7 @@ import css from './title.module.css';
 import PropTypes from 'prop-types';
 
 export const Title = ({ text }) => {
-  if (Title) {
-    return <h2 className={css.title}>{text}</h2>;
-  }
+  return text && <h2 className={css.title}>{text}</h2>;
 };
 
 Title.propTypes = {
